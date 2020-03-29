7e000fb42a88c04006ec74a7d71bc7baeb6b0cb3
sayarkh <47250683+sayarkh@users.noreply.github.com>
2020-03-29 17:17:11 +0600

    Add files via upload

---
+++ summer practice/js/script.js
@@ -0,0 +1,10 @@
+//var h1= document.querySelector{'h1'}
+//h1.style.color = 'Pink'
+//var li = document.getElementById('one')
+//var liItem = document.getElementsByClassName('li-item')
+//liItem[0]
+//li.style.fontWeight = 800
+function back(){
+	var btn = document.body
+	btn.style.background = 'red'
+}
